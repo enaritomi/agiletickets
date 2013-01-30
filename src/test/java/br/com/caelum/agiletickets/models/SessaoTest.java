e781866b44220a6a4ea5011880c8e1a8e4de2ca1
Edlene <enaritomi@gmail.com>
2013-01-29 23:01:10 -0200

    Bug Fix

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,22 +1,37 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
-
+	Sessao sessao;
+	
+	@Before
+	public void criaSessao(){
+		sessao= new Sessao();
+	}
+	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+		//Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
+	public void deveVender1ingressoSeHa1vaga() throws Exception {
+		//Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(1);
+
+        Assert.assertTrue(sessao.podeReservar(1));
+	}
+
+	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+		//Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -24,7 +39,7 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
+		//Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
